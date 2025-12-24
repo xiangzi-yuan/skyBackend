53c1751a7bef7c3a5af8baacf9c07e17ff9ffdb1
an yu <1778442487@qq.com>
2025-12-24 21:21:35 +0800

    查询菜品

---
+++ sky-pojo/src/main/java/com/sky/vo/DishPageVO.java
@@ -0,0 +1,14 @@
+package com.sky.vo;
+
+public class DishPageVO {
+    private Long id;
+    private String name;
+    private Long categoryId;
+    private String categoryName;
+    private BigDecimal price;
+    private String image;
+    private Integer status;
+    
+    private LocalDateTime updateTime;
+    // 与rm一致
+}
